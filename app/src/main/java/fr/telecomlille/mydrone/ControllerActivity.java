b044a5f1b6b4cd17599fb456a79da7a46afa50fe
Hélène <helene.grosse@gmail.com>
2016-10-28 16:50:50 +0200

    Ajout des fonctions de décollage et atterrissage

--- app/src/main/java/fr/telecomlille/mydrone/ControllerActivity.java
+++ app/src/main/java/fr/telecomlille/mydrone/ControllerActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 
+import com.parrot.arsdk.arcommands.ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM;
 import com.parrot.arsdk.arcontroller.ARCONTROLLER_DEVICE_STATE_ENUM;
 import com.parrot.arsdk.arcontroller.ARCONTROLLER_DICTIONARY_KEY_ENUM;
 import com.parrot.arsdk.arcontroller.ARCONTROLLER_ERROR_ENUM;
@@ -16,6 +19,7 @@ import com.parrot.arsdk.arcontroller.ARControllerException;
 import com.parrot.arsdk.arcontroller.ARDeviceController;
 import com.parrot.arsdk.arcontroller.ARDeviceControllerListener;
 import com.parrot.arsdk.arcontroller.ARDeviceControllerStreamListener;
+import com.parrot.arsdk.arcontroller.ARFeatureARDrone3;
 import com.parrot.arsdk.arcontroller.ARFeatureCommon;
 import com.parrot.arsdk.arcontroller.ARFrame;
 import com.parrot.arsdk.ardiscovery.ARDISCOVERY_PRODUCT_ENUM;
@@ -33,6 +37,7 @@ public class ControllerActivity extends AppCompatActivity implements ARDeviceCon
     private ARDeviceController mDeviceController;
     private BebopVideoView mVideoView;
     private ProgressBar mBatteryLevel;
+    private ARCONTROLLER_DEVICE_STATE_ENUM mState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +59,38 @@ public class ControllerActivity extends AppCompatActivity implements ARDeviceCon
         } catch (ARControllerException e) {
             e.printStackTrace();
         }
+
+        Button takeoffButton = (Button) findViewById(R.id.btn_takeoff);
+        takeoffButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mState.equals(ARCONTROLLER_DEVICE_STATE_ENUM.ARCONTROLLER_DEVICE_STATE_RUNNING)
+                        && ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_LANDED.equals(getPilotingState())) {
+                    mDeviceController.getFeatureARDrone3().sendPilotingTakeOff();
+                }
+            }
+        });
+        Button landButton = (Button) findViewById(R.id.btn_land);
+        landButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mState.equals(ARCONTROLLER_DEVICE_STATE_ENUM.ARCONTROLLER_DEVICE_STATE_RUNNING)
+                        && (ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_HOVERING.equals(getPilotingState())
+                        || ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_FLYING.equals(getPilotingState()))) {
+                    mDeviceController.getFeatureARDrone3().sendPilotingLanding();
+                }
+            }
+        });
+        Button emergencyButton = (Button) findViewById(R.id.btn_emergency);
+        emergencyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mDeviceController != null && mState.equals(ARCONTROLLER_DEVICE_STATE_ENUM.ARCONTROLLER_DEVICE_STATE_RUNNING)) {
+                    mDeviceController.getFeatureARDrone3().sendPilotingEmergency();
+                }
+            }
+        });
+
     }
 
     @Override
@@ -97,11 +134,31 @@ public class ControllerActivity extends AppCompatActivity implements ARDeviceCon
         return device;
     }
 
+    private ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM getPilotingState() {
+        ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM flyingState = ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.eARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_UNKNOWN_ENUM_VALUE;
+        if (mDeviceController != null) {
+            try {
+                ARControllerDictionary dict = mDeviceController.getCommandElements(ARCONTROLLER_DICTIONARY_KEY_ENUM.ARCONTROLLER_DICTIONARY_KEY_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED);
+                if (dict != null) {
+                    ARControllerArgumentDictionary<Object> args = dict.get(ARControllerDictionary.ARCONTROLLER_DICTIONARY_SINGLE_KEY);
+                    if (args != null) {
+                        Integer flyingStateInt = (Integer) args.get(ARFeatureARDrone3.ARCONTROLLER_DICTIONARY_KEY_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE);
+                        flyingState = ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.getFromValue(flyingStateInt);
+                    }
+                }
+            } catch (ARControllerException e) {
+                e.printStackTrace();
+            }
+        }
+        return flyingState;
+    }
+
     @Override
     public void onStateChanged(ARDeviceController deviceController,
                                ARCONTROLLER_DEVICE_STATE_ENUM newState,
                                ARCONTROLLER_ERROR_ENUM error) {
-        switch (newState) {
+        mState = newState;
+        switch (mState) {
             case ARCONTROLLER_DEVICE_STATE_RUNNING:
                 Log.d(TAG, "State changed: RUNNING");
                 deviceController.getFeatureARDrone3().sendMediaStreamingVideoEnable((byte) 1);
@@ -141,13 +198,18 @@ public class ControllerActivity extends AppCompatActivity implements ARDeviceCon
             // if the command received is a battery state changed
             if (commandKey == ARCONTROLLER_DICTIONARY_KEY_ENUM.ARCONTROLLER_DICTIONARY_KEY_COMMON_COMMONSTATE_BATTERYSTATECHANGED) {
                 ARControllerArgumentDictionary<Object> args = elementDictionary.get(ARControllerDictionary.ARCONTROLLER_DICTIONARY_SINGLE_KEY);
-
                 if (args != null) {
                     int batValue = (int) args.get(ARFeatureCommon.ARCONTROLLER_DICTIONARY_KEY_COMMON_COMMONSTATE_BATTERYSTATECHANGED_PERCENT);
 
                     // do what you want with the battery level
                     mBatteryLevel.setProgress(batValue);
                 }
+            }
+        } else if (commandKey == ARCONTROLLER_DICTIONARY_KEY_ENUM.ARCONTROLLER_DICTIONARY_KEY_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED) {
+            ARControllerArgumentDictionary<Object> args = elementDictionary.get(ARControllerDictionary.ARCONTROLLER_DICTIONARY_SINGLE_KEY);
+            if (args != null) {
+                Integer flyingStateInt = (Integer) args.get(ARFeatureARDrone3.ARCONTROLLER_DICTIONARY_KEY_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE);
+                ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM flyingState = ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.getFromValue(flyingStateInt);
             }
         } else {
             Log.e(TAG, "elementDictionary is null");
